87a01040de8da2871810f77c365578cc8662ea04
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-12-04 07:39:47 +0000

    feat(skeleton): check if skeleton exists before anything

--- skeleton/skeleton.sh
+++ skeleton/skeleton.sh
@@ -5,7 +5,7 @@ CMD=$(basename $0)
 TEMP_DIR=$(mktemp -d)
 # TEMP_DIR=/workspaces/tmp
 SKELETON=$1
-TARGET_DIR=$SKELETON
+TARGET=$SKELETON
 SKELETON_BRANCH=skeleton
 WORKDIR=$(pwd)
 
@@ -17,7 +17,7 @@ fi
 
 # if second argument is specified, use it as target
 if [ ! -z "$2" ]; then
-    TARGET_DIR=$2
+    TARGET=$2
 fi
 
 # select custom repo with -r option
@@ -55,20 +55,25 @@ if [ "$REPO_BRANCH" == "$SKELETON_BRANCH" ]; then
     echo -e "\033[33mWarning:\033[0m current branch ($REPO_BRANCH) is equal to skeleton branch ($SKELETON_BRANCH)"
 fi
 
-TARGET_EXISTS=0
-if [ -e $REPO_ROOT/$SKELETON ]; then
+if [ -e $REPO_ROOT/$TARGET ]; then
     TARGET_EXISTS=1
 fi
 
-if [ -z "$TARGET_EXISTS" ]; then
-    echo -e "Adding \033[33m$SKELETON\033[0m from \033[33m$REPO\033[0m to $TARGET_DIR in \033[33mskeleton\033[0m branch and merging to \033[33m$REPO_BRANCH\033[0m"
+if [ -z $TARGET_EXISTS ]; then
+    echo -e "Adding \033[33m$SKELETON\033[0m from \033[33m$REPO\033[0m to \033[33m$TARGET\033[0m in \033[33mskeleton\033[0m branch and merging to \033[33m$REPO_BRANCH\033[0m"
 else
-    echo -e "Updating \033[33m$SKELETON\033[0m from \033[33m$REPO\033[0m to $TARGET_DIR in \033[33mskeleton\033[0m branch and merging to \033[33m$REPO_BRANCH\033[0m"
+    echo -e "Updating \033[33m$SKELETON\033[0m from \033[33m$REPO\033[0m to \033[33m$TARGET\033[0m in \033[33mskeleton\033[0m branch and merging to \033[33m$REPO_BRANCH\033[0m"
 fi
 
 echo ""
 
 git clone --quiet --depth=1 $REPO $TEMP_DIR/skeleton 1>/dev/null
+
+if [ ! -e "$TEMP_DIR/skeleton/$SKELETON" ]; then
+    echo "Skeleton $SKELETON not found in $REPO"
+    rm -fr $TEMP_DIR 1>/dev/null
+    exit 1
+fi
 
 if git show-ref --quiet refs/heads/$SKELETON_BRANCH; then
     git switch $SKELETON_BRANCH 1>/dev/null
@@ -87,10 +92,10 @@ for file in $UNTRACKED_FILES; do
 done
 
 # remove target directory and copy new skeleton
-rm -fr $REPO_ROOT/$TARGET_DIR 1>/dev/null
-cp -r $TEMP_DIR/skeleton/$SKELETON $REPO_ROOT/$TARGET_DIR 1>/dev/null
+rm -fr $REPO_ROOT/$TARGET 1>/dev/null
+cp -r $TEMP_DIR/skeleton/$SKELETON $REPO_ROOT/$TARGET 1>/dev/null
 
-git add -A $REPO_ROOT/$TARGET_DIR
+git add -A $REPO_ROOT/$TARGET
 
 # return untracked files back
 for file in $UNTRACKED_FILES; do
@@ -109,9 +114,9 @@ fi
 
 # merge changes to original branch
 if [ -z "$TARGET_EXISTS" ]; then
-    git commit -m "feat(skeleton): add $TARGET_DIR from $REPO#$SKELETON"
+    git commit -m "feat(skeleton): add $TARGET from $REPO#$SKELETON"
 else
-    git commit -m "feat(skeleton): update $TARGET_DIR from $REPO#$SKELETON"
+    git commit -m "feat(skeleton): update $TARGET from $REPO#$SKELETON"
 fi
 
 # cleanup
@@ -119,4 +124,4 @@ rm -fr $TEMP_DIR 1>/dev/null
 
 git switch $REPO_BRANCH 1>/dev/null
 
-git merge --no-ff $SKELETON_BRANCH
+git merge --no-ff --no-edit $SKELETON_BRANCH
