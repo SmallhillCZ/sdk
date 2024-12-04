8938434ad1a89f79035aeb622141fde79ef1142f
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-12-04 01:18:34 +0000

    fix(skeleton): do not commit untracked merge files other than current skeleton

--- skeleton/skeleton.sh
+++ skeleton/skeleton.sh
@@ -3,10 +3,8 @@
 REPO=https://github.com/smallhillcz/sdk
 CMD=$(basename $0)
 TEMP_DIR=$(mktemp -d)
-
-# get command used to run this script
-
 SKELETON=$1
+
 # error if skeleton is not specified
 if [ -z "$SKELETON" ]; then
     echo "Please specify skeleton folder"
@@ -33,24 +31,25 @@ while getopts ":r:h" opt; do
     esac
 done
 
-CURRENT_BRANCH=$(git branch --show-current)
+REPO_ROOT=$(git rev-parse --show-toplevel)
+REPO_BRANCH=$(git branch --show-current)
 
 TARGET_DIR=$SKELETON
 TARGET_EXISTS=$(ls -A | grep $TARGET_DIR)
 
 if [ -z "$TARGET_EXISTS" ]; then
-    echo -e "Adding \033[33m$SKELETON\033[0m from \033[33m$REPO\033[0m to \033[33mskeleton\033[0m branch and merging to \033[33m$CURRENT_BRANCH\033[0m"
+    echo -e "Adding \033[33m$SKELETON\033[0m from \033[33m$REPO\033[0m to \033[33mskeleton\033[0m branch and merging to \033[33m$REPO_BRANCH\033[0m"
 else
-    echo -e "Updating \033[33m$SKELETON\033[0m from \033[33m$REPO\033[0m to \033[33mskeleton\033[0m branch and merging to \033[33m$CURRENT_BRANCH\033[0m"
+    echo -e "Updating \033[33m$SKELETON\033[0m from \033[33m$REPO\033[0m to \033[33mskeleton\033[0m branch and merging to \033[33m$REPO_BRANCH\033[0m"
 fi
 
 git clone --depth=1 $REPO $TEMP_DIR/skeleton 2>/dev/null
 
-git switch skeleton 2>/dev/null || git checkout -b skeleton 2>/dev/null
+git switch skeleton 2>/dev/null || git switch -c skeleton 2>/dev/null
 
-cp -r $TEMP_DIR/skeleton/$SKELETON ./ 2>/dev/null
+cp -r $TEMP_DIR/skeleton/$SKELETON $REPO_ROOT/ 2>/dev/null
 
-git add -A
+git add -A $REPO_ROOT/$SKELETON
 
 if [ -z "$TARGET_EXISTS" ]; then
     git commit -m "feat(skeleton): add $SKELETON from $REPO"
@@ -60,6 +59,6 @@ fi
 
 rm -fr $TEMP_DIR 2>/dev/null
 
-git switch $CURRENT_BRANCH 2>/dev/null
+git switch $REPO_BRANCH 2>/dev/null
 
 git merge --no-ff skeleton
