66ed5eacdbfbdd6886c7e18642ac1ac9b1952e52
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-12-04 04:55:06 +0000

    feat(nestjs): openapi

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { Config } from "./config";
+import { registerOpenAPI } from "./openapi";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	const config = app.get(Config);
+
+	registerOpenAPI(app, config);
 
 	await app.listen(config.server.port, config.server.host);
 }
