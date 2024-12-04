699a162a427d3049547e7da1d93adb5a65ac9365
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-12-04 01:08:54 +0000

    feat(skeleton): state when updating and when adding

--- skeleton/skeleton.sh
+++ skeleton/skeleton.sh
@@ -35,7 +35,14 @@ done
 
 CURRENT_BRANCH=$(git branch --show-current)
 
-echo -e "Adding \033[33m$SKELETON\033[0m from \033[33m$REPO\033[0m to \033[33mskeleton\033[0m branch and merging to \033[33m$CURRENT_BRANCH\033[0m"
+TARGET_DIR=$SKELETON
+TARGET_EXISTS=$(ls -A | grep $TARGET_DIR)
+
+if [ -z "$TARGET_EXISTS" ]; then
+    echo -e "Adding \033[33m$SKELETON\033[0m from \033[33m$REPO\033[0m to \033[33mskeleton\033[0m branch and merging to \033[33m$CURRENT_BRANCH\033[0m"
+else
+    echo -e "Updating \033[33m$SKELETON\033[0m from \033[33m$REPO\033[0m to \033[33mskeleton\033[0m branch and merging to \033[33m$CURRENT_BRANCH\033[0m"
+fi
 
 git clone --depth=1 $REPO $TEMP_DIR/skeleton 2>/dev/null
 
@@ -44,7 +51,12 @@ git switch skeleton 2>/dev/null || git checkout -b skeleton 2>/dev/null
 cp -r $TEMP_DIR/skeleton/$SKELETON ./ 2>/dev/null
 
 git add -A
-git commit -m "feat(skeleton): add $SKELETON from $REPO"
+
+if [ -z "$TARGET_EXISTS" ]; then
+    git commit -m "feat(skeleton): add $SKELETON from $REPO"
+else
+    git commit -m "feat(skeleton): update $SKELETON from $REPO"
+fi
 
 rm -fr $TEMP_DIR 2>/dev/null
 
