c89e81d7f2e1ec2f77ca0b141c3bc4aef82ca6ba
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-12-04 01:25:18 +0000

    fix(skeleton): correct skeleton exists logic

--- skeleton/skeleton.sh
+++ skeleton/skeleton.sh
@@ -4,6 +4,7 @@ REPO=https://github.com/smallhillcz/sdk
 CMD=$(basename $0)
 TEMP_DIR=$(mktemp -d)
 SKELETON=$1
+SKELETON_BRANCH=skeleton
 
 # error if skeleton is not specified
 if [ -z "$SKELETON" ]; then
@@ -12,16 +13,20 @@ if [ -z "$SKELETON" ]; then
 fi
 
 # select custom repo with -r option
-while getopts ":r:h" opt; do
+while getopts ":r:hb:" opt; do
     case ${opt} in
     r)
         REPO=$OPTARG
+        ;;
+    b)
+        SKELETON_BRANCH=$OPTARG
         ;;
     h)
         echo "Usage:"
         echo "  $CMD [options] <skeleton>"
         echo "Options:"
-        echo "  -r <repo>  Use custom repo"
+        echo "  -r <repo>  Use custom repo (default: https://github.com/smallhillcz/sdk)"
+        echo "  -b <branch>  Use custom branch for skeletons (default: skeleton)"
         exit 0
         ;;
     \?)
@@ -34,8 +39,10 @@ done
 REPO_ROOT=$(git rev-parse --show-toplevel)
 REPO_BRANCH=$(git branch --show-current)
 
-TARGET_DIR=$SKELETON
-TARGET_EXISTS=$(ls -A | grep $TARGET_DIR)
+TARGET_EXISTS=0
+if [ -d $REPO_ROOT/$SKELETON ]; then
+    TARGET_EXISTS=1
+fi
 
 if [ -z "$TARGET_EXISTS" ]; then
     echo -e "Adding \033[33m$SKELETON\033[0m from \033[33m$REPO\033[0m to \033[33mskeleton\033[0m branch and merging to \033[33m$REPO_BRANCH\033[0m"
